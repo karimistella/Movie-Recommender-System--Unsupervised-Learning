819b1916696df303ee07022adf4d79e96427f13e
lnjoctrey <30340090+lnjoctrey@users.noreply.github.com>
2022-05-05 01:53:10 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -29,12 +29,16 @@
 import streamlit as st
 from PIL import Image
 
+# library to evaluate strings containing python literals
+from ast import literal_eval
+
 # Data handling dependencies
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
 import os
+import re
 
 # Custom Libraries
 from utils.data_loader import load_movie_titles
@@ -45,7 +49,7 @@ from recommenders.content_based import content_model
 import seaborn as sns
 
 
-movies_meta = 'resources/data/movies.csv'
+movies_meta = 'resources/data/movies_metadata.csv'
 # Data Loading
 title_list = load_movie_titles('resources/data/movies.csv')
 
@@ -116,24 +120,141 @@ def main():
     if page_selection == "Solution Overview":
         st.title("Solution Overview")
         st.write("Describe your winning approach on this page")
-        st.header("Movies")
-        TopRated = st.radio("Top Rated", 
-        ('Movies', 'Directors', 'Actors'))
+        st.header("Movies Data")
+    
 
         #EDA
-        if TopRated == "Movies":
+        if st.checkbox("Data Preview"):
             def eda_data(dataset):
                 df = pd.read_csv(os.path.join(dataset))
                 return df
-            if st.checkbox("Preview Dataset"):
+
+            #Previwing Dataset
+            st.markdown("""
+            To see the first five rows of the dataset select "Head" and 
+            to see the last 5 rows of the dataset select "Tail"
+            """)
+            if st.checkbox("Raw Uncleaned Data"):
                 data = eda_data(movies_meta)
-                if st.button("Head"):
+                options = st.radio("", ("Head", "Tail"))
+                if options == "Head":
                     st.write(data.head())
+                elif options == "Tail":
+                    st.write(data.tail())
+
+            #Cleaning Data
+
+            meta_copy = eda_data(movies_meta).copy(deep=True)
+            meta_copy.drop_duplicates(inplace=True)
+
+            # Agenda 1: Column Belongs_to_collection should be converted to a boolean field (True or False)
+            meta_copy['belongs_to_collection'] = meta_copy['belongs_to_collection'].apply(lambda row: True if type(row) != float else False )
+            
+            x = "[{'id': 12, 'name': 'Adventure'},{'id': 14, 'name': 'Fantasy'},{'id': 10751, 'name': 'Family'}]"
+            x = literal_eval(x)
+
+            # Agenda 2: Extract the genres from column genres as they are store in dictionaries in a list
+
+            def decompose(text, key='name'):
+                
+                try:
+                    # check if text is np.nan 
+                    if type(text) == float:
+                        decomposed_text = text
+                        
+                    #apply literal_exal to each row of data
+                    eval_text = literal_eval(text)
+
+                    # get the name key of each dictionary in the list
+                    # store extracted name in a list
+                    # join each item in the list into a string
+                    decomposed_text = ' '.join([dictionary[key].replace(' ', '_') for dictionary in eval_text]).lower()
+                
+                except (ValueError, TypeError):
+                    decomposed_text = np.nan
+                    
+                return decomposed_text
+
+            # apply the decompose function on the genres column.
+            meta_copy['genres'] = meta_copy['genres'].apply(decompose)
+
+
+
+            # Agenda 3: Extract the digits from the imdb_id column and rename the column to imdbId
+
+            re_pattern = '\d+' # regex to extract 1 or more digits
+
+            # applying regex, search through every row text, find every digits
+            # in the text then return the group of texts if the row is not 'null'
+            meta_copy['imdb_id'] = meta_copy['imdb_id'].apply(lambda row: re.search(re_pattern, row).group() if type(row) == str else np.nan).astype('float32') # convert the column to dtype 'int32'
+
+            # rename the 'imdb_id' to 'imdbId' 
+            meta_copy = meta_copy.rename(columns={'imdb_id': 'ImdbId'})
+
+            # Agenda 4: Convert popularity to float32
+            meta_copy['popularity'] = pd.to_numeric(meta_copy['popularity'], downcast='float', errors='coerce')
+            # Agenda 5: Extract production_companies and production_countries
+
+            boolean = np.where((meta_copy['production_companies'] == True) | (meta_copy['production_companies'] == 'True') | (meta_copy['production_companies'] == False) | (meta_copy['production_companies'] == 'False') | (meta_copy['production_companies'] == 'nan') | (meta_copy['production_companies'] == np.nan)) 
+
+            data = meta_copy.iloc[boolean]
+            meta_copy['production_companies'] = meta_copy['production_companies'].apply(lambda x: np.nan if x in ('', ' ', 'True', True, 'False', False) else x).apply(decompose)
+
+            meta_copy['production_countries'] = meta_copy['production_countries'].apply(lambda x: np.nan if x in ('', ' ', 'True', True, 'False', False) else x).apply(decompose)
+            
+            # Agenda 7: Extract language from spoken_languages
+
+            # here, we change the key of the decompose function
+            # because we want the encoding of the language
+            # not the language name itself
+            meta_copy['spoken_languages'] = meta_copy['spoken_languages'].apply(decompose, args=('iso_639_1',))
+
+            # Agenda 8: Replace Zeros in budget with np.nan and make column a numerical column
+
+            meta_copy['budget'] = meta_copy['budget'].apply(lambda row: np.nan if row == '0' else row)
+            meta_copy['budget'] = pd.to_numeric(meta_copy['budget'], downcast='float', errors='coerce')
+
+            # Agenda 9: Convert title to lowercase
+
+            # Convert title column to lowercase
+            meta_copy['title'] = meta_copy['title'].str.lower()
+
+            if st.checkbox("Cleaned Data"):
+                data2 = meta_copy
+                options2 = st.radio("", ("First Five", "Last Five"))
+                if options2 == "First Five":
+                    st.write(data2.head())
+                elif options2 == "Last Five":
+                    st.write(data2.tail())
+            
+            if st.checkbox("Data Summary"):
+                st.markdown("""
+                Below is a brief summary of our movie dataset. 
+                """)
+                st.write(meta_copy.describe())
 
             
+            st.header("Data Visualization")
+            # Question 1: how many movies belong to a collection?
+
+            collection = meta_copy['belongs_to_collection'].value_counts()
+
+            ax = sns.barplot(collection.index, collection.values)
+            for p in ax.patches:
+                percentage = '{:.1f}%'.format(100 * p.get_height()/len(meta_copy['belongs_to_collection'].tolist()))
+                x = p.get_x() + p.get_width()
+                y = p.get_height()
+                ax.annotate(percentage, (x, y),ha='right', fontsize=15)
+            plt.show()
+
+
+            
+
+
+
 
             #axes = df.plot.bar(x='original_title', y='budget')
-            
+
 
     
     if page_selection == "Meet The Team":
